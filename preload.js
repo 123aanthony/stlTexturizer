c4d56c08cb186151a361f6f523498f7e73f2dfb5
Anthony Andre <getonthegaz@gmail.com>
2026-06-05 07:06:24 +0200

    Stabilize texture slot duplication and multislot workflow
    Instantane d archive de la branche locale fix/multislot-quality (cb9cd4a), MOINS dist/
    (build electron-builder de 260 Mo commite par erreur). Sans ascendance :
    le binaire vivait dans l historique autant que dans l arbre.

---
+++ preload.js
@@ -0,0 +1,26 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('bumpforgeElectron', {
+  isElectron: true,
+
+  saveBlob: (options) =>
+    ipcRenderer.invoke('save-blob', options),
+
+  saveFile: (options) =>
+    ipcRenderer.invoke('save-file', options),
+
+  openFile: (options) =>
+    ipcRenderer.invoke('open-file', options),
+
+  chooseDirectory: () =>
+    ipcRenderer.invoke('choose-directory'),
+
+  saveSetting: (key, value) =>
+    ipcRenderer.invoke('save-setting', key, value),
+
+  loadSetting: (key) =>
+    ipcRenderer.invoke('load-setting', key),
+
+  scanTextureLibrary: (folderPath) =>
+    ipcRenderer.invoke('scan-texture-library', folderPath)
+});
